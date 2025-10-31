13357ea96ca9a07b8f8359e4990e3f51affed374
zblaylock <zack.blaylock@sendsafely.com>
2025-10-31 16:31:10 -0400

    Merge Queue Test 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Merge Queue Test 2</h1>
+      <p>Start building your application here.</p>
+    </div>
+  );
+}
+
+export default App;
+
